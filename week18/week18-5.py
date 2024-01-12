9477340b59b7549b141103324bacd1a5763e8fa9
gillhung <gill09190918@gmail.com>
2024-01-12 11:33:06 +0800

    week18

---
+++ week18/week18-5.py
@@ -0,0 +1,12 @@
+#chapter3 資料储存容器
+a=[100,200] #方括號 list 陣列、串列、列表
+c,d=a #逗號，左邊 a,b tuple
+print(c)
+print(d)
+s='Hello'
+t=[100:200,300:400] #大括號、字典 裡面會有 左邊:右邊
+t={300} #大括號 也可以是集合 裡面沒有冒號
+
+#chapter 4 if判斷
+#chapter 5 for迴圈 for i in range(N): 或 for i in s:
+#chapter 6 函式 def gcd(a,b): 這樣寫
